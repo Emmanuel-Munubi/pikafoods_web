64ab75607a733f2e479827fa7565a0484b63330f
Emmanuel-Munubi <manumunubi@gmail.com>
2022-11-22 15:02:32 +0300

    added pages and components

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import LeftBar from "../components/LeftBar";
+import RightBar from "../components/RightBar";
+import NavBar from "../components/Navbar";
+
+export { LeftBar, RightBar, NavBar };
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import Home from "../pages/Home";
+import FoodDetails from "../pages/FoodDetails";
+import Layout from "../pages/Layout";
+import Orders from "../pages/Orders";
+
+export { Home, FoodDetails, Layout, Orders };
